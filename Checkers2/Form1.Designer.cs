8134eda1bbd85505416122ab77056d145b450e30
Nulllink <47364651+Nulllink@users.noreply.github.com>
2022-02-09 13:05:04 +0300

    First pull

---
+++ Checkers2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Checkers2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(33, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(680, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Новая игра";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(631, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(727, 471);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 550);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(969, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 572);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Checkers2/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkers2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region global
+        bool player = true;
+        Image ch_black = Properties.Resources._4;
+        Image ch_white = Properties.Resources._5;
+        int[,] matrix = new int[8, 8];
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = "White";
+            int size = panel1.Width / 8;
+            for (int x = 0; x < 8; x++)
+            {
+                for(int y = 0; y < 8; y++)
+                {
+                    PictureBox pic = new PictureBox
+                    {
+                        Name = $"{x}_{y}",
+                        BorderStyle = BorderStyle.FixedSingle,
+                        Width = size,
+                        Height = size,
+                        Location = new Point(x * size, y * size),
+                        BackColor = Color.White,
+                        SizeMode = PictureBoxSizeMode.StretchImage,
+                    };
+                    if(x % 2 == 1 && y % 2 == 0 || x % 2 == 0 && y % 2 == 1)
+                    {
+                        pic.BackColor = Color.Gray;
+                        if (y < 3)
+                        {
+                            matrix[x, y] = 3;
+                        }
+                        else if (y > 4)
+                        {
+                            matrix[x, y] = 1;
+                        }
+                    }
+                    pic.MouseClick += Pic_MouseClick;
+                    panel1.Controls.Add(pic);
+                }
+            }
+            draw();
+        }
+
+        private void draw()
+        {
+            for(int x = 0; x < 8; x++)
+            {
+                for(int y = 0; y < 8; y++)
+                {
+                    if (x % 2 == 1 && y % 2 == 0 || x % 2 == 0 && y % 2 == 1)
+                    {
+                        PictureBox pic = panel1.Controls[$"{x}_{y}"] as PictureBox;
+                        pic.BackColor = Color.Gray;
+                        switch (matrix[x, y])
+                        {
+                            case 0:
+                                pic.Image = null;
+                                break;
+                            case 1:
+                                pic.Image = ch_white;
+                                break;
+
+                            case 3:
+                                pic.Image = ch_black;
+                                break;
+                            case 5:
+                                pic.BackColor = Color.Green;
+                                break;
+                        }
+                        if (matrix[x, y] < 0)
+                        {
+                            pic.BackColor = Color.Yellow;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Pic_MouseClick(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = sender as PictureBox;
+            string[] s = pic.Name.Split('_');
+            int x = int.Parse(s[0]);
+            int y = int.Parse(s[1]);
+            label1.Text = $"Click on {pic.Name}";
+
+            if (matrix[x, y] > 4)
+            {
+                move(x,y);
+                return;
+            }
+            //old_pic = pic;
+            if (player && matrix[x, y] > 0 && matrix[x, y] < 3)
+            {
+                right_click(pic, x,y);
+            }
+            else if (!player && matrix[x, y] > 2 && matrix[x, y] < 5)
+            {
+                right_click(pic, x,y);
+            }
+        }
+        private void right_click(PictureBox pic, int x, int y)
+        {
+            clear_matrix();
+            matrix[x, y] *= -1;
+            steps(x,y);
+            draw();
+        }
+
+        private void clear_matrix()
+        {
+            for(int x = 0; x < 8; x++)
+            {
+                for(int y=0; y < 8; y++)
+                {
+                    if (x % 2 == 1 && y % 2 == 0 || x % 2 == 0 && y % 2 == 1)
+                    {
+                        matrix[x, y] = Math.Abs(matrix[x, y]);
+                        if(matrix[x,y] == 5)
+                        {
+                            matrix[x, y] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void delete_chess()
+        {
+            
+        }
+
+        private void steps(int x, int y)
+        {
+
+            if(matrix[x,y] == -1 || matrix[x,y] == -2)
+            {
+                make_green(x - 1, y - 1);
+                make_green(x + 1, y - 1);
+            }
+            if (matrix[x, y] == -3 || matrix[x, y] == -4)
+            {
+                make_green(x - 1, y + 1);
+                make_green(x + 1, y + 1);
+            }
+        }
+
+        private void make_green(int x, int y)
+        {
+            if (x >= 0 && x < 8 && y >= 0 && y < 8)
+            {
+                
+                if (matrix[x,y] == 0)
+                {
+                    matrix[x, y] = 5;
+                }
+                else
+                {
+                    if (player && (matrix[x,y] == 3 || matrix[x,y] == 4))
+                    {
+                        make_red(x,y);
+                    }
+                    if (!player && (matrix[x, y] == 1 || matrix[x, y] == 2))
+                    {
+                        make_red(x,y);
+                    }
+                }
+            }
+        }
+        private void make_red(int r_x,int r_y)
+        {
+            int x = 0, y = 0;
+            for (; x < 8; x++)
+            {
+                for (y = 0; y < 8; y++)
+                {
+                    if (matrix[x, y] < 0)
+                    {
+                        x += 8;
+                        break;
+                    }
+                }
+            }
+            x -= 9;
+            x = x - r_x;
+            y = y - r_y;
+            matrix[x, y] = 6;
+            //int delta_x = blocks[blocks.Count - 1].x - blocks[0].x;
+            //int delta_y = blocks[blocks.Count - 1].y - blocks[0].y;
+            //int x = blocks[blocks.Count - 1].x + delta_x;
+            //int y = blocks[blocks.Count - 1].y + delta_y;
+            //PictureBox pic = panel1.Controls[$"{x}_{y}"] as PictureBox;
+            //Block b = new Block(pic, x, y);
+            //blocks.Add(b);
+            //if(pic.Image == null)
+            //{
+            //    pic.BackColor = Color.Red;
+            //}
+        }
+
+        private void move(int g_x, int g_y)
+        {
+            delete_chess();
+            int x = 0, y = 0;
+            for(; x < 8; x++)
+            {
+                for(y=0; y < 8; y++)
+                {
+                    if(matrix[x,y] < 0)
+                    {
+                        x += 8;
+                        break;
+                    }
+                }
+            }
+            x -= 9;
+            matrix[g_x, g_y] = matrix[x, y];
+            matrix[x, y] = 0;
+            clear_matrix();
+            draw();
+            if (player)
+            {
+                player = false;
+                toolStripStatusLabel1.Text = "Black";
+            }
+            else
+            {
+                player = true;
+                toolStripStatusLabel1.Text = "White";
+            }
+        }
+    }
+}
